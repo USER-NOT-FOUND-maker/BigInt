79d6302971bbed0dc866f5700a6caa48995e98aa
USER-NOT-FOUND-maker <iamdemariam@gmail.com>
2025-05-27 17:43:34 +0100

    added a README for sumn reason

--- BigInt.cpp
+++ BigInt.cpp
@@ -4,3 +4,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+// we WILL be using little endian
+
+typedef struct{
+        int* ArrOfInts;
+        unsigned long long NumberOfDigits;
+
+}
+
+int main(){
+        return 69;
+}
