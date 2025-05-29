acf5fc3aeb147b41bf10f6c2adadbba9e2cbfdd6
USER-NOT-FOUND-maker <iamdemariam@gmail.com>
2025-05-29 11:07:35 +0100

    added a bit more functionality

--- BigInt.cpp
+++ BigInt.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 #define SIZEOFINT 4
+#define MAXINTSIZE 4294967295
 
 // we WILL be using little endian
 
@@ -20,21 +21,26 @@ char IntToDigt(int digit){
         return ((char) digit) + 48;
 }
 
-string IntToString(unsigned int number){
-
-}
-
-/*
-string DisplayIntArr(int arr[]){
-        string Display = "[";
-
-        const SIZEOFARR = sizeof(arr);
-
-        for (size_t i = 0; i < SIZEOFARR; i++){
-                Display.append()
+void DisplayIntArr(unsigned int arr[], unsigned long long length){
+        cout << "[";
+        for (unsigned long long i = 0; i < length; i++){
+                cout << arr[i];
+                if (i < (length - 1)){
+                        cout << ", ";
+                }
         }
+        cout << "]" << endl;
 }
-*/
+
+bool IsOverflow(unsigned int Num1, unsigned int Num2, char op){
+        if ( op != '+' && op != '-') { cout << "entered op was " << op << endl << endl; throw runtime_error("fucking retard put in a valid op next time (+ or -)"); }
+
+        unsigned int Result;
+
+        if (op == '+') { Result = Num1 + Num2; return Result < Num1; }
+        else { Result = Num1 - Num2; return Result > Num1; }
+}
+
 void CreateBigint(BigInt* NumberAddress){
         NumberAddress->ArrOfInts = (unsigned int *) malloc(SIZEOFINT);
         NumberAddress->NumberOfDigits = 1;
@@ -42,19 +48,70 @@ void CreateBigint(BigInt* NumberAddress){
         NumberAddress->ArrOfInts[0] = 0;
 }
 
-void PrintBigintInfo(BigInt* NumberAddress){
+void SetBigIntVal(BigInt * PointerToNum, unsigned int Num){
+        free(PointerToNum->ArrOfInts);
+        PointerToNum->NumberOfDigits = 1;
+        PointerToNum->ArrOfInts = (unsigned int*) malloc(SIZEOFINT);
+        PointerToNum->ArrOfInts[0] = Num;
+}
 
-        cout << "IntegerArrays (pointer) = " << NumberAddress->ArrOfInts << "\n\nNumberOfDigits = " << NumberAddress->NumberOfDigits << "\n\nREPRESENTATION = " << NumberAddress->representation << endl;
+int CmpNums(BigInt NumberOne,BigInt NumberTwo){
+        if (NumberOne.NumberOfDigits > NumberTwo.NumberOfDigits) { return -1; }
+        else if (NumberOne.NumberOfDigits < NumberTwo.NumberOfDigits) { return 1; }
+
+        const unsigned long long NumberOfDigits = NumberOne.NumberOfDigits - 1;
+
+        if (NumberOne.ArrOfInts[NumberOfDigits] == NumberTwo.ArrOfInts[NumberOfDigits]) { return 0; }
+        else if (NumberOne.ArrOfInts[NumberOfDigits] > NumberTwo.ArrOfInts[NumberOfDigits]) { return -1; }
+        else { return 1; }
+}
+
+BigInt AddNums(BigInt NumberOne,BigInt NumberTwo){
+        BigInt Result;
+        CreateBigint(&Result);
+
+        return Result;
+}
+
+void PrintBigintInfo(BigInt* NumberAddress){
+        cout << "Integer array = ";
+        DisplayIntArr(NumberAddress->ArrOfInts,NumberAddress->NumberOfDigits);
+        cout << "\nInteger array pointer = " << NumberAddress->ArrOfInts;
+        cout << "\n\nNumberOfDigits = " << NumberAddress->NumberOfDigits << "\n\nREPRESENTATION = " << NumberAddress->representation << endl << endl;
 }
 
 int main(){
+        cout << "DISCLAIMER, USING 'SetBigIntVal' ONLY WORKS WHEN YOU PASS IN AN UNSIGNED INTEGER THAT FITS IN AN UNSIGNED INTEGER" << endl << endl << endl;
+
         BigInt TestNum;
+        BigInt TestNum2;
         CreateBigint(&TestNum);
+        CreateBigint(&TestNum2);
+
+        SetBigIntVal(&TestNum,4250);
+        SetBigIntVal(&TestNum2,4250);
+
+        cout << "Expected result is 0, result is " << CmpNums(TestNum,TestNum2) << endl;
+
+
+        SetBigIntVal(&TestNum,0);
+        SetBigIntVal(&TestNum2,0);
+
+
+        cout << "Expected result is 0, result is " << CmpNums(TestNum,TestNum2) << endl;
+
+        SetBigIntVal(&TestNum,5);
+        SetBigIntVal(&TestNum2,10);
         
-        cout << "DEBUG INFO OF TESTNUM\n------------------------\n\n";
-        PrintBigintInfo(&TestNum);
+        cout << "Expected result is 1, result is " << CmpNums(TestNum,TestNum2) << endl;
         
-        cout << "string of 213 = " << IntToString((unsigned int) 213) << endl; 
+        SetBigIntVal(&TestNum,10);
+        SetBigIntVal(&TestNum2,5);
+
+
+        cout << "Expected result is -1, result is " << CmpNums(TestNum,TestNum2) << endl;
+        
+
 
         return 69;
 }
