e307140b4828e15b778b0a2b2f3056808fa38cbe
USER-NOT-FOUND-maker <iamdemariam@gmail.com>
2025-10-26 18:44:53 +0000

    sumn wrong with the addnums function, future mes issue

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 #define UINT32 unsigned int
+#define INTSCIIVAL 48
 
 typedef struct{
 	char* number; // still just a pointer to the START of the number, to get the whole number you have to use ANOTHER pointer to go through the whole list.
@@ -46,10 +47,31 @@ UINT32 Strlen(char* str){
 	return len;
 }
 
+char* reverseString(char* str){
+	const UINT32 len = Strlen(str);
+	char* strcp = malloc(Strlen(str));
+	strcpy(strcp,str);
+	
+	if (len == 0 || len == 1) { return str; }
+
+	char* start = strcp;
+	char* end = start + len - 1;
+
+	char temp;
+
+	while (start < end){
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		end--;
+		start++;
+	}
+
+	return strcp;
+}
+
 bool isNum(char* val){
 	char* ptr = val;
-	printf("\nval = %p",val);
-
 
 	if ( (int) *ptr > 57 || (int) *ptr < 48) { return false; }
 
@@ -60,7 +82,10 @@ bool isNum(char* val){
 	return true;
 }
 
-void setNumVal(num* numPtr,char* value){
+void setNumVal(num* numPtr,char* value){	
+	if (!isNum(value)) { return; }
+	if (Strlen(value) == 0) { return; }
+
 	numPtr->number = realloc(numPtr->number,Strlen(value) + 1);
 	numPtr->digits = Strlen(value);
 	
@@ -84,6 +109,8 @@ bool sanityCheck(num* number){
 	if (len != number->digits) { return false; }
 
 	if (number-> end != (number->number + number->digits - 1)) { return false; }
+	
+	if (!isNum(number->number)) { return false; }
 
 	return true;
 }
@@ -114,15 +141,94 @@ void leftPad(char** str, char pad, UINT32 n,bool guard){
 	free(strcp);
 }
 
+int cmpNums(num NOne, num NTwo){
+	if (NOne.digits > NTwo.digits) { return -1; }
+	else if (NOne.digits < NTwo.digits) { return 1; }
+
+	for (UINT32 i = 0; i < NTwo.digits; i++){ // this absolutely has to change (to support numbers with more than 2^32 digits, will do once addNums is working
+		int codeOne = (int) NOne.number[i];
+		int codeTwo = (int) NTwo.number[i];
+
+		if (codeOne > codeTwo){
+			return -1;
+		}
+		else if (codeOne < codeTwo){
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 num addNums(num NOne, num NTwo){
+
+	if (!(sanityCheck(&NOne) && sanityCheck(&NTwo))) { return; }
+
 	if (NOne.digits > NTwo.digits){
 		NTwo.digits = NOne.digits;
 		NTwo.number = realloc(NTwo.number,NTwo.digits + 1);
+		leftPad(&NTwo.number,'0',NTwo.digits - Strlen(NTwo.number),true);
 	}
+	else if (NOne.digits < NTwo.digits){
+		NOne.digits = NTwo.digits;
+		NOne.number = realloc(NOne.number,NOne.digits + 1);
+		leftPad(&NOne.number,'0',NOne.digits - Strlen(NOne.number),true);
+	}
+	
+	int carry; // stores the number used to carry in addition
+	char digitOne; // stores the character digit of the first number
+	char digitTwo; // stores the character digit of the second number
+	int sum; // stores the result of adding digitOneN and digitTwoN
+	int digitOneN; // digitOne but integer
+	int digitTwoN; // digitTwo but integer
+
+	char* NOneCP = malloc(NOne.digits);
+	char* NTwoCP = malloc(NTwo.digits);
+	strcpy(NOneCP,NOne.number);
+	strcpy(NTwoCP,NTwo.number);
+	NOneCP = reverseString(NOneCP);
+	NTwoCP = reverseString(NTwoCP);
+	
+	const UINT32 len = Strlen(NOneCP);
+	char* res = malloc(len + 1); // stores the result of the addition, +1 is needed to account for carries in the final digit addition
+	char* resI = res;
+
+	for (UINT32 i = 0; i < len; i++){
+		digitOne = NOneCP[i];
+		digitTwo = NTwoCP[i];
+
+		digitOneN = (int) digitOne - INTSCIIVAL;
+		digitTwoN = (int) digitTwo - INTSCIIVAL;
+		
+		printf("\ndigitOne = %c\ndigitTwo = %c\ndigitOneN = %d\ndigitTwoN = %d\n",digitOne,digitTwo,digitOneN,digitTwoN);
+
+		sum = digitOneN + digitTwoN;
+
+		if (sum > 9) { sum -= 10; carry = 1; }
+
+		*(resI + i) = (char) (sum + carry + INTSCIIVAL);
+		printf("\nresI + i (dereferenced) = %c",*(resI + i));
+		printf("\n(char) sum + carry = %c",(char) (sum + carry));
+		printf("\nin the loop, res = %s",res);
+
+		carry = 0;
+	}
+	res = reverseString(res);
+
+	printf("\n%s\n",res);
+
 }
 
 int main(){
 	num testNum;
 	init(&testNum);
+	num testNum2;
+	init(&testNum2);
+	
+	setNumVal(&testNum,"10000");
+	setNumVal(&testNum2,"100000000");
+
+	addNums(testNum,testNum2);
+
 	return 0;
 }
